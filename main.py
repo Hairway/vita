c1111a410800539f2e8b379473850c6b5b3ce9c5
Dima Fomin <36900526-hairway7@users.noreply.replit.com>
2024-10-02 11:33:11 +0000

    button fix

--- main.py
+++ main.py
@@ -91,7 +91,7 @@ def send_water_reminder():
         message = reminder_messages[datetime.now().hour % len(reminder_messages)]
         bot.send_message(last_chat_id, message)
         markup = telebot.types.InlineKeyboardMarkup()
-        confirm_button = telebot.types.InlineKeyboardButton("✅ Выпил воду", callback_data="confirm_water")
+        confirm_button = telebot.types.InlineKeyboardButton("✅ Выпила воду", callback_data="confirm_water")
         markup.add(confirm_button)
         bot.send_message(last_chat_id, "Не жульничай, солнышко. Нажми тогда, когда выпила водички", reply_markup=markup)
 
